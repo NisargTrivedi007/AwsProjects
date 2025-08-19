49aa0ab4b97482b08337d5197566b924d31d0abc
Nisarg Trivedi <nisargpc733@gmail.com>
2025-08-19 23:13:22 +0530

    changed app version to dotnet 8

--- AwsTestApi/Program.cs
+++ AwsTestApi/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
 builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions("AWSS3"));
 builder.Services.AddAWSService<IAmazonS3>();
 
@@ -15,18 +15,18 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.MapScalarApiReference(options =>
-    {
-        //options.with
-    });
+    //app.MapOpenApi();
+    
 }
 app.MapS3Endpoints();
-
+app.MapScalarApiReference(options =>
+{
+    //options.with
+});
 //app.UseHsts();
 app.UseHttpsRedirection();
 
-app.MapGet("testurl", () =>
+app.MapGet("", () =>
 {
     return "This is a test URL";
 });
